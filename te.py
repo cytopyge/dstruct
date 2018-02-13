02b09cb26803997190090c7604b297127ce775a6
Your Name <you@example.com>
2018-02-13 09:38:12 +0000

    made a store

--- te.py
+++ te.py
@@ -1,23 +1,25 @@
 import json
 
-def saveValue(a_dict):
-    with open('test.json') as f:
-        data = json.load(f)
-    data.update(a_dict)
-    with open('test.json', 'w') as f:
-        json.dump(data, f)
+class Store:
+    #def __init__(self):
+    def saveValue(self, a_dict):
+        with open('test.json') as f:
+            data = json.load(f)
+        data.update(a_dict)
+        with open('test.json', 'w') as f:
+            json.dump(data, f)
 
-def giveAll():
-    with open('test.json') as f:
-        data = json.load(f)
-    return data
+    def giveAll(self):
+        with open('test.json') as f:
+            data = json.load(f)
+        return data
 
-def getValue(val):
-    with open('test.json') as f:
-        data = json.load(f)
-    return data[val]
+    def getValue(self, val):
+        with open('test.json') as f:
+            data = json.load(f)
+        return data[val]
 
-saveValue({'port1': 'ccccc'})
-# print(giveAll())
+opslag = Store()
+opslag.saveValue({'port1': 'leeg'})
 
-print(getValue("ouwe_key"))
+print(opslag.getValue("port1"))
