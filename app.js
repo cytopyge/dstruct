6c1b3cd5623e0c97429dc85e7a6661141c776920
zoutepopcorn <zoutepopcorn@hotmail.com>
2018-02-22 09:50:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const udev = require("udev");
 const monitor = udev.monitor();
+const exec = require('child_process').exec;
 console.clear();
-// console.log(udev.list()); // this is a long list :)
+
+function wipe(sdb, id) {
+  console.log(`whipe ${sdb}`);
+  // const CMD = `dd command`;
+  const CMD = `echo "dd commando wiping ${sdb}"`;
+  let dd = exec(CMD);
+  // DD output
+  dd.stdout.on('data', function(data) {
+    // TODO: LEDS
+    console.log(`--> ${data}`);
+  });
+}
 
 monitor.on('add', function(device) {
+  console.log("add monitor");
+  // New device
   if (device.DEVNAME.indexOf("/dev/sd") == 0 && device.DEVTYPE.indexOf("disk") == 0) {
     console.log(device.DEVNAME);
-    console.log(device.ID_SERIAL_SHORT);
-    console.log(device);
+    console.log(device.DEVNAME, device.ID_SERIAL_SHORT);
+    // console.log(device);
+    wipe(device.ID_SERIAL_SHORT)
   }
 });
 
